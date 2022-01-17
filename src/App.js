769a1a02ab348a2728f2d1271bcd95f93475d7fc
Parker Mackenzie <wparkermackenzie@outlook.com>
2022-01-17 11:12:40 -0500

    207South-9 Completed building out foundation
    Removed some of the files which were introduced by the create
    react app, will put these back in later as necessary. Now stripped
    to the foundation can build up again with my own content.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
+/*
+Return multiple elements in a fragment without adding extra
+nodes to the Document Object Model.
+<> </> is a shortcut for <React.Fragment> </React.Fragment>
+*/
+
+/**
+ * Entry point function for the react application
+ * @returns 
+ */
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    /*
+     An HTML Router is used to forward an incoming request to the 
+     correct component, allowing an application to have multiple 
+     views. For example, if the application www.myapp.com a request
+     is made to www.myapp.com/myorders the application finds
+     the path called /myorders appended to the URL. The router
+     checks to see if there is a component associated with
+     /myorders. If the application has a handler for this path,
+     its component (function/class) is invoked. The react router allows
+     this to happen while preserving user state.
+
+     Anything outside of the BrowserRouter router component will be
+     rendered on every page. As well as anything just before the 
+     Routes. It is unclear to me if one method is prefered over the
+     other or why. 
+
+     More information: https://reactrouter.com/web/guides/quick-start 
+    */
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+        </Routes>
+      </BrowserRouter>
+
+  );
+}
+
+function Home() {
+  return(
+    <h1> Hello this is the foundation for my new website</h1>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+/*
+  This is where the fun starts. Call ReactDom.render with the following
+  arguments to start the React application:
+  - element :
+    The element describes what will be described on the screen. In this case 
+    it is JSX which contains a component like notation which tells React to 
+    run the entire application in strict mode; this highlights potential
+    problems in the application. 
+    The component App is loaded to kick off the excitement.
+  - container : 
+    The application is rendered within the root of the HTML document (top level)
+*/
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
